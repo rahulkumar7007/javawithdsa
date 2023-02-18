f48b6437d4012642bca468738abca7b85a3db4ab
rahulkumar7007 <jkr8960@gmail.com>
2023-02-18 19:41:03 +0530

    first java string

--- string.java
+++ string.java
@@ -4,13 +4,34 @@
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         // System.out.print("Enter a string: ");
-        String str = "PWSKILLS";
+        // String str = "PWSKILLS";
         // String str = sc.nextLine();
-        StringBuilder sb = new StringBuilder(str);
-        sb.reverse();
-        System.out.println("Reversed : " + sb.toString());
+        // StringBuilder sb = new StringBuilder(str);
+        // sb.reverse();
+        // System.out.println("Reversed : " + sb.toString());
+
+
+    //     String str = "Hello";
+ 
+    //     str = "Bye";
+   
+    //    System.out.println(str);
+
+    int i;
+
+    for (i = 1; i <= 12; i += 2) {
+
+       if (i == 8) {
+
+        System.out.println(i);
+
+       break;
+
+      }
+
+    }
     }
 }
 
